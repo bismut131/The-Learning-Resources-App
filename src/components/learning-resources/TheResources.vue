2b5dd42805682d334cab605af7da718aa05bdae0
Murman Vadachkoria <murman.vadachkoria@rockartech.com>
2022-02-20 13:41:54 +0400

    added delete and add Resource feature

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -3,7 +3,9 @@
     <base-button @click='setSelectedTab("stored-resources")' :mode='storedResButtonMode'>Stored Resources</base-button>
     <base-button @click='setSelectedTab("add-resource")' :mode='addResButtonMode'>Add Resource</base-button>
   </base-card>
-  <component :is='selectedTab'></component>
+  <keep-alive>
+    <component :is='selectedTab'></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -44,12 +46,27 @@ export default {
   },
   provide() {
     return {
-      resources: this.storedResources
+      resources: this.storedResources,
+      addResource: this.addResource,
+      removeResource: this.removeResource
     };
   },
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+    addResource(title, description, link) {
+      this.storedResources.push({
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: link
+      });
+      this.setSelectedTab('stored-resources');
+    },
+    removeResource(resId) {
+      const indexResource = this.storedResources.findIndex(res => resId === res.id);
+      this.storedResources.splice(indexResource, 1);
     }
   }
 };
